48bd376a127ea6e759029b92d43a33e2788c726a
kirangps <kirankumargps@gmail.com>
2021-08-05 10:57:55 +0530

    Updates

---
+++ precache-manifest.83160ef9c71070ddff28467d3778bb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335ef3e5db1f62616f929250b0dd7e96",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "652d0f48648103d113f4",
+    "url": "/expense-tracker-react/static/css/main.571e6405.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/expense-tracker-react/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/expense-tracker-react/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652d0f48648103d113f4",
+    "url": "/expense-tracker-react/static/js/main.d5df6f69.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
